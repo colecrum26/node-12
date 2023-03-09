012a16e42a765174dfd0e4b228429c4cd664b586
colecrum26 <cwcrumrine@gmail.com>
2023-03-09 10:42:25 -0600

    overwrote named variables and logged

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
@@ -1,1 +1,21 @@
 // Write Your JavaScript Code Here
+
+var personName = "Cole";
+var pets = 1;
+var funFact = "Climbed Mt. Kilimanjaro";
+
+personName = "AJ";
+pets = 1;
+funFact = "AJ has a Boston Terrier";
+
+console.log(personName);
+console.log(pets);
+console.log(funFact);
+
+console.log("My name is");
+console.log("I have pet(s)");
+console.log("Fun fact:");
+
+console.log("My name is " + personName + ".");
+console.log(personName + " has " + pets + " pet(s). ");
+console.log("Fun fact: " + funFact + ".");
