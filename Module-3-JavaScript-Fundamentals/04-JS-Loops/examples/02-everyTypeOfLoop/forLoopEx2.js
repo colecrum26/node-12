30aaccef045623abfd933abe7ef8714b20e3b93f
colecrum26 <cwcrumrine@gmail.com>
2023-03-14 14:00:00 -0500

    working with basic for loops

--- Module-3-JavaScript-Fundamentals/04-JS-Loops/examples/02-everyTypeOfLoop/forLoopEx2.js
+++ Module-3-JavaScript-Fundamentals/04-JS-Loops/examples/02-everyTypeOfLoop/forLoopEx2.js
@@ -1,4 +1,4 @@
-for (let bottlesOnTheWall = 99; bottlesOnTheWall <= 109; bottlesOnTheWall++) {
+for (let bottlesOnTheWall = 99; bottlesOnTheWall <= 115; bottlesOnTheWall++) {
     if (bottlesOnTheWall) {
       console.log(`${bottlesOnTheWall} bottles of milk on the wall...`);
     } else {
