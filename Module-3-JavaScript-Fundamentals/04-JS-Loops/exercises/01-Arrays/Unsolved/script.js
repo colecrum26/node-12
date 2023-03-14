0ae16a2cd7ca8a6edd6498d4038f02ee078aba52
colecrum26 <cwcrumrine@gmail.com>
2023-03-14 12:02:09 -0500

    worked on array exercise

--- Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/01-Arrays/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/01-Arrays/Unsolved/script.js
@@ -1,1 +1,12 @@
 // WRITE YOUR CODE HERE
+let studentNames = ["Andre", "Karl", "Rashida", "Olivia"];
+
+console.log(studentNames.length);
+
+console.log("Welcome to the class " + studentNames);
+
+studentNames[0] = "Jeff";
+
+if (studentNames[0] === "Jeff"){
+    console.log("Jeff is in class")
+}
