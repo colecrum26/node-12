e1b481979e998bb96c44955f46f5b1cfee1c6407
colecrum26 <cwcrumrine@gmail.com>
2023-03-17 09:57:25 -0500

    more practice with forEach

--- Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/02-forEach/forEach.js
+++ Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/02-forEach/forEach.js
@@ -1,29 +1,35 @@
-
 // Exercise 1
 let arr1 = ["Andre", "Karl", "Rashida", "Olivia"];
 
-arr1.forEach((value) => {
-    console.log(value);
-})
-
-
+// arr1.forEach((value) => {
+//   // console.log(value);
+// });
 
 // Exercise 2
 
 let arr2 = ["Bears", "Giraffes", "Penguins", "Meerkats"];
 
-arr2.forEach((value) => {
-    console.log(value)
-})
-
-
+// arr2.forEach((value) => {
+//   // console.log(value)
+// });
 
 // Exercise 3
 
-let bottlesOnTheWall = 99; bottlesOnTheWall <= 115; bottlesOnTheWall++ {
-    if (bottlesOnTheWall) {
-      console.log(`${bottlesOnTheWall} bottles of milk on the wall...`);
-    } else {
-      console.log(`No more bottles of milk on the wall!`);
-    }
-  }
+// for (let bottlesOnTheWall = 99; bottlesOnTheWall <= 115; bottlesOnTheWall++){
+//     if (bottlesOnTheWall) {
+//       console.log(`${bottlesOnTheWall} bottles of milk on the wall...`);
+//     } else {
+//       console.log(`No more bottles of milk on the wall!`);
+//     }
+//   }
+
+let arr3 = []
+for(let i = 1; i <= 99; i++) {
+  arr3.push(i)
+};
+arr1.reverse()
+console.log(arr3);
+
+arr3.forEach((value) => {
+  console.log(value);
+});
