5ad3b5461fcd69b7b0b922b921fcfa893665f15a
colecrum26 <cwcrumrine@gmail.com>
2023-03-17 09:27:55 -0500

    worked review exercises one & two

---
+++ Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/02-forEach/forEach.js
@@ -0,0 +1,29 @@
+
+// Exercise 1
+let arr1 = ["Andre", "Karl", "Rashida", "Olivia"];
+
+arr1.forEach((value) => {
+    console.log(value);
+})
+
+
+
+// Exercise 2
+
+let arr2 = ["Bears", "Giraffes", "Penguins", "Meerkats"];
+
+arr2.forEach((value) => {
+    console.log(value)
+})
+
+
+
+// Exercise 3
+
+let bottlesOnTheWall = 99; bottlesOnTheWall <= 115; bottlesOnTheWall++ {
+    if (bottlesOnTheWall) {
+      console.log(`${bottlesOnTheWall} bottles of milk on the wall...`);
+    } else {
+      console.log(`No more bottles of milk on the wall!`);
+    }
+  }
