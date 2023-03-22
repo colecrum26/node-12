1bc5b5eb5b348906cdb7846a1d22b19c7a9115a3
colecrum26 <cwcrumrine@gmail.com>
2023-03-22 09:40:44 -0500

    morning OOP review

--- Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
+++ Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
@@ -1,7 +1,14 @@
 // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
-
-// TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+function Developer(name, tech) {
+  this.name = name;
+  this.tech = tech;
+  // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+  this.introduction = () => {
+    console.log(`Hello, my name is ${this.name} and I love ${this.tech}.`);
+  };
+}
 
 // TODO: Create a new object using the 'Developer' constructor
-
+const aj = new Developer("AJ", "JavaScript")
 // TODO: Call the 'introduction()' method on the new object
+aj.introduction();
