23c7f5f7184827d821f7ac318209b715a0367f69
colecrum26 <cwcrumrine@gmail.com>
2023-03-22 10:36:57 -0500

    commented additional way

--- Module-04-Advanced-JS/04-JS-DOM/exercises/01-Traverse-DOM/Unsolved/assets/js/script.js
+++ Module-04-Advanced-JS/04-JS-DOM/exercises/01-Traverse-DOM/Unsolved/assets/js/script.js
@@ -3,5 +3,6 @@ var articlesDiv = document.getElementById('articles');
 var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
+// You can also access the element(s) by creating another variable and tagging its ID
 articlesDiv.children[0].style.fontSize = '50px';
 headerDiv.children[0].style.color = 'white';
