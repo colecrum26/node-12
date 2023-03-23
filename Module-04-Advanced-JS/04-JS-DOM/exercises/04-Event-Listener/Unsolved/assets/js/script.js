8b3ee5105df00e9ed52d56ad4e3dca6d5fe73b91
colecrum26 <cwcrumrine@gmail.com>
2023-03-23 09:46:38 -0500

    morning review

--- Module-04-Advanced-JS/04-JS-DOM/exercises/04-Event-Listener/Unsolved/assets/js/script.js
+++ Module-04-Advanced-JS/04-JS-DOM/exercises/04-Event-Listener/Unsolved/assets/js/script.js
@@ -3,10 +3,36 @@ var incrementEl = document.querySelector("#increment");
 var decrementEl = document.querySelector("#decrement");
 var countEl = document.querySelector("#count");
 
+// Refactored Solution
 function setCounterText() {
-  countEl.textContent = count;
+  console.log(this);
+  if (this == incrementEl){
+    count++;
+    countEl.textContent = count;
+  } else {
+    if (0 < count) {
+      count--;
+      countEl.textContent = count;
+    }
+  }
 }
 
+incrementEl.addEventListener("click", setCounterText);
+decrementEl.addEventListener("click", setCounterText);
+
+// My Solution
 // TODO: Add event listener to increment button
+// incrementEl.addEventListener("click", function setCounterText() {
+//   count++;
+//   countEl.textContent = count;
+// })
 
 // TODO: Add event listener to decrement button 
+// decrementEl.addEventListener("click", function setCounterText(){
+//   count--;
+//   countEl.textContent = count;
+//   if (count < 0){
+//     count = 0;
+//     // end;
+//   }
+// })
