fd55ca4de92d48e1d86e1d7ccb742a97442cb559
colecrum26 <cwcrumrine@gmail.com>
2023-03-27 09:34:00 -0500

    morning review with fetch API

--- Module-04-Advanced-JS/08-JS-FetchAPI/exercises/01-Parse_JSON/Unsolved/assets/js/script.js
+++ Module-04-Advanced-JS/08-JS-FetchAPI/exercises/01-Parse_JSON/Unsolved/assets/js/script.js
@@ -1,5 +1,9 @@
 // TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
-var requestUrl = 'https://api.github.com/repos/{owner}/{repo}/issues';
+// Can set global variables and use string interpolation to populate URL
+// let owner = "twitter";
+// let repo = "chill";
+
+var requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
 
 fetch(requestUrl)
   .then(function (response) {
@@ -7,7 +11,9 @@ fetch(requestUrl)
   })
   .then(function (data) {
     console.log('Github Repo Issues \n----------');
-    console.log(data);
     // TODO: Loop through the response
     // TODO: Console log each issue's URL and each user's login
+    for (let i = 0; i < data.length; i++){
+      console.log(data[i].url, data[i].user.login);
+    }
   });
