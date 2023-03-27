59b79d87ed96f4da3bae050eb56d080d616aaaab
colecrum26 <cwcrumrine@gmail.com>
2023-03-27 11:59:55 -0500

    import maths exercise

--- Module-04-Advanced-JS/09-NPM&ES6/exercises/01-Modularization/Unsolved/index.js
+++ Module-04-Advanced-JS/09-NPM&ES6/exercises/01-Modularization/Unsolved/index.js
@@ -1,7 +1,29 @@
 // TODO: Import `maths.js`
+const maths = require("./maths.js");
 
 // TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
-
+operation = process.argv[2];
+numOne = parseInt(process.argv[3]);
+numTwo = parseInt(process.argv[4]);
+// console.log(operation);
+// console.log(numOne);
+// console.log(numTwo);
 // TODO: Create a `switch` statement that accepts an `operation` parameter
+switch (operation) {
+  case "sum":
+    console.log("sum = " + maths.sum(numOne, numTwo));
+    break;
+  case "difference":
+    console.log("difference = " + maths.difference(numOne, numTwo));
+    break;
+  case "product":
+    console.log("product = " + maths.product(numOne, numTwo));
+    break;
+  case "quotient":
+    console.log("quotient = " + maths.quotient(numOne, numTwo));
+    break;
+  default:
+    console.log("Please check your input.");
+}
 // and each `case` uses the corresponding `maths` method
 // to perform each math operation on the two numbers, `numOne` and `numTwo`
