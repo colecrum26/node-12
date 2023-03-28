800d7f15158c8a3639cbdd4477b42f55b1a5acaf
colecrum26 <cwcrumrine@gmail.com>
2023-03-28 11:36:57 -0500

    brought in exercise src

---
+++ Module-05-React/02-Component-State&Props/exercises/01-Render-Element/Unsolved/src/components/HelloDiv.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HelloDiv() {
+// TODO: Fix the return statement so that it renders an `h1` heading tag that introduces the creator
+return (
+  <div>
+  <h1>Hello, I am Cole, your creator.</h1>
+  <ul>My Interests Include:
+  <li>Coding</li>
+  <li>Skiing</li>
+  <li>Hiking</li>
+  </ul>
+  </div>
+  );
+}
+// TODO: Fix the export statement
+// Export statements are ES6 syntax that tell Javascript what "thing" we want to make available outside of this file
+export default HelloDiv;
