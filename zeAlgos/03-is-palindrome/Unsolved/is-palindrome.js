17924139a002b1dfad8590f97c84914b255e5821
colecrum26 <cwcrumrine@gmail.com>
2023-04-05 15:04:07 -0500

    isPalindrome script

--- zeAlgos/03-is-palindrome/Unsolved/is-palindrome.js
+++ zeAlgos/03-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,11 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+    let reverseStr = str.split("").reverse().join("");
+    console.log(reverseStr);
+    if (reverseStr == str){
+        return true;
+    } else {
+        return false;
+    }
+};
