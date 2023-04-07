0a51171b6c5e803d0b4beb3dded57758f54887d8
colecrum26 <cwcrumrine@gmail.com>
2023-04-07 10:02:54 -0500

    morning review - vowel count algo

--- zeAlgos/04-vowel-count/Unsolved/vowel-count.js
+++ zeAlgos/04-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,14 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function (str) {
+  let vowels = 0;
+  str
+    .toLowerCase()
+    .split("")
+    .map((e) => {
+      if (e == "a" || e == "e" || e == "i" || e == "o" || e == "u") {
+        vowels++;
+      }
+    });
+    return vowels;
+};
